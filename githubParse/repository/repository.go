e0552fb9f0a8681bfa742f0869a7792ece558348
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-10-02 21:05:04 +0200

    🔥 delete not requested filed

--- githubParse/repository/repository.go
+++ githubParse/repository/repository.go
@@ -7,13 +7,12 @@ import (
 const RepositoryURL = "/repos"
 
 type Repository struct {
-	Name         string
-	FullName     string `json:"full_name"`
-	ID           int    `json:"id"`
-	NodeID       string `json:"node_id"`
-	URL          string `json:"url"`
-	HTMLURL      string `json:"html_url"`
-	Description  string
-	Owner        *User
-	Contributors []string
+	Name        string
+	FullName    string `json:"full_name"`
+	ID          int    `json:"id"`
+	NodeID      string `json:"node_id"`
+	URL         string `json:"url"`
+	HTMLURL     string `json:"html_url"`
+	Description string
+	Owner       *User
 }
