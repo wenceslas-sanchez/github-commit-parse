65b4474a3299889d6bac69840c6ca66eab6632fa
WenceslasSANCHEZ <wenceslas.sanchez@compass-ft.com>
2022-10-16 15:04:28 +0200

    🎨 add users path

--- githubParse/app/server.go
+++ githubParse/app/server.go
@@ -2,14 +2,27 @@ package app
 
 import (
 	"githubParse/githubParse/app/root"
+	"githubParse/githubParse/app/users"
+	"githubParse/githubParse/commit"
 	"githubParse/githubParse/organization"
 	"log"
 	"net/http"
+	"sync"
 )
 
-func RunApplication(organization *organization.Organization) {
+type ApplicationData struct {
+	*organization.Organization
+	*commit.NestedCounter
+	USerCount *map[string]int
+}
+
+func RunApplication(data *ApplicationData) {
 	mux := http.NewServeMux()
 
-	mux.HandleFunc("/", root.Handler(organization))
+	mux.HandleFunc("/", root.Handler(data.Organization))
+	mux.Handle("/users/",
+		&users.UserHandler{Organization: data.Organization,
+			RWMutex: &sync.RWMutex{}})
+
 	log.Fatal(http.ListenAndServe("localhost:8000", mux))
 }
